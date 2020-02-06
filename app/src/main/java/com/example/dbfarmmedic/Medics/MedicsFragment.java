0e4a77c2911d8ff539aceb37a07128539924caf0
Misael Bances <bancesbusiness@gmail.com>
2020-02-06 17:05:04 -0500

    medics_data_clasification

--- app/src/main/java/com/example/dbfarmmedic/Medics/MedicsFragment.java
+++ app/src/main/java/com/example/dbfarmmedic/Medics/MedicsFragment.java
@@ -1,6 +1,15 @@
 package com.example.dbfarmmedic.Medics;
 
+import android.widget.ListView;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.dbfarmmedic.DataMedic.MedicsDbHelper;
+
 public class MedicsFragment extends Fragment {
+    public static final int REQUEST_UPDATE_DELETE_MEDICS = 2;
+    private static final String ARG_TIPO_MEDICS_ID = "medicsId";
+    private MedicsDbHelper mMedicsDbHelper;
+
+    private ListView mMedicsList;
 }
