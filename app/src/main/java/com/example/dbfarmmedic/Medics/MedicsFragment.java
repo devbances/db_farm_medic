f5d0c9763d6fc295b7d7446bed79c2094c18434a
Misael Bances <bancesbusiness@gmail.com>
2020-02-07 12:14:19 -0500

    medics_data_clasification

--- app/src/main/java/com/example/dbfarmmedic/Medics/MedicsFragment.java
+++ app/src/main/java/com/example/dbfarmmedic/Medics/MedicsFragment.java
@@ -1,10 +1,16 @@
 package com.example.dbfarmmedic.Medics;
 
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.dbfarmmedic.DataMedic.MedicsDbHelper;
+import com.example.dbfarmmedic.R;
 
 public class MedicsFragment extends Fragment {
     public static final int REQUEST_UPDATE_DELETE_MEDICS = 2;
@@ -12,4 +18,35 @@ public class MedicsFragment extends Fragment {
     private MedicsDbHelper mMedicsDbHelper;
 
     private ListView mMedicsList;
+    private MedicsCursorAdapter mMedicsAdapter;
+    private String mIdTipo;
+
+    public MedicsFragment(){
+
+    }
+
+    public static MedicsFragment newInstance(String idTipo){
+        MedicsFragment fragment = new MedicsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TIPO_MEDICS_ID, idTipo);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null){
+            mIdTipo = getArguments().getString(ARG_TIPO_MEDICS_ID);
+        }
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View root = inflater.inflate(R.layout.fragment_medics, container, false);
+
+
+    }
 }
